11a43be8b8c16b7cab0098625361e38f909cdef3
Ayodeji <ayodejiayankola@gmail.com>
2020-01-03 16:47:37 +0100

    refactored user database and home view

--- messaging_app/resources/views/home.blade.php
+++ messaging_app/resources/views/home.blade.php
@@ -2,22 +2,27 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <div class="row">
+        <div class="col-3 p-5">
+    <img src="{{asset('images/logo.jpg')}}" class="rounded-circle">
+               
         </div>
+        <div class="col-9 pt-5">
+            <div><h1>          {{ Auth::user()->username}} </h1></div>
+            <div class="d-flex" >
+                <div class="pr-3"><strong>10</strong> posts</div>
+                <div class="pr-3"><strong>2500</strong> followers</div>
+                <div class="pr-3"><strong>210</strong> following</div>
+            </div>
+            <div>Freecodecamp.org</div>
+            <div>If the route accepts parameters, you may pass them as the second argument to the method:</div>
+            <div><a href="#">Freecodecamp.org</a></div>
+        </div>
+    </div>
+    <div class="row pt-4">
+        <div class="col-4 "><img src="{{asset('images/1.jpg')}}" class="w-100" /></div>
+        <div class="col-4 "><img src="{{asset('images/2.jpg')}}"  class="w-100" /></div>
+        <div class="col-4 "><img src="{{asset('images/3.jpg')}}" class="w-100" /></div>
     </div>
 </div>
 @endsection
--- messaging_app/resources/views/layouts/app.blade.php
+++ messaging_app/resources/views/layouts/app.blade.php
@@ -24,7 +24,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    
+                <div ><span > <img src="{{asset('images/logo.jpg')}}" style=" padding-right:10px; border-right: 1px solid black; height:30px;"></span>    Messaging App</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -51,7 +52,7 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->username}} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
